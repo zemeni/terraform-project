3b555d62f6dae217eec73129483386f1a4152553
Babu <neupanebabu828@gmail.com>
2023-09-24 01:50:32 +0930

    cli-setup-n-configure

---
+++ aws-cli-setup/cli-setup.sh
@@ -0,0 +1,18 @@
+## install aws cli ###
+##sudo apt install awscli
+
+## configure aws cli ###
+# Go to AWS Console -> create programmatic user with admin access
+# Go to security credentials -> create access key -> download csv
+
+## CLI
+## aws configure
+## provide access key, secret key and region name and output format (json)
+
+
+## now verify the aws configure
+## cat ~/.aws/config
+## cat ~/.aws/credentials
+
+
+## aws iam list-users
